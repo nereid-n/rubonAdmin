743ae64d2f00791c5d77fec3fa58314a37b57ead
iskybow <kjionmd@gmail.com>
2019-04-25 11:08:48 +0300

    edits

---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-navigation-drawer
+      fixed
+      :value="drawer"
+      app
+  >
+    <v-list dense>
+      <v-list-tile v-for="item in items"
+                   :to="item.to"
+                   :key="item.name"
+      >
+        <v-list-tile-content>
+          <v-list-tile-title>{{item.name}}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    name: "Navigation",
+    data: () => ({
+      items: [
+        {
+          name: 'Объявления',
+          to: '/cabinet/ads'
+        },
+        {
+          name: 'Добавить объявление',
+          to: '/cabinet/ad/add'
+        },
+        {
+          name: 'Профиль',
+          to: '/cabinet/profile'
+        },
+        {
+          name: 'Аккаунт',
+          to: '/cabinet/account'
+        },
+        {
+          name: 'Соцсети',
+          to: '/cabinet/networks'
+        }
+      ]
+    }),
+    props: {
+      drawer: {
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .v-navigation-drawer {
+    z-index: 5;
+  }
+</style>
---
+++ src/components/layout/Toolbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-toolbar color="blue" dark fixed app>
+    <v-toolbar-side-icon @click.stop="toggleDrawer"></v-toolbar-side-icon>
+    <v-toolbar-title>
+      {{$route.meta.title}}
+      <template v-if="$route.meta.id">
+        {{$route.params.id}}
+      </template>
+    </v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: "Toolbar",
+    data: () => ({
+      drawer: true
+    }),
+    methods: {
+      toggleDrawer() {
+        this.drawer = !this.drawer;
+        this.$emit('toggleDrawer', this.drawer);
+      }
+    },
+    created() {
+      if (window.innerWidth < 1265) {
+        this.drawer = false;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .v-toolbar--fixed {
+    z-index: 4;
+  }
+</style>
