59f1c34ab9dc37544b491c4d853f0a0b1d504d24
iskybow <kjionmd@gmail.com>
2019-04-25 13:34:54 +0300

    ac pop-up

--- src/views/settings/Account.vue
+++ src/views/settings/Account.vue
@@ -10,6 +10,14 @@
                v-validate="input.rules"
     />
     <v-btn color="info" class="ma-0" @click.prevent="submit">Сохранить</v-btn>
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card class="pt-1">
+        <v-card-title class="headline justify-center text-xs-center pb-0 pt-3">Сохранено успешно!</v-card-title>
+        <v-card-actions class="justify-center pb-4">
+          <v-btn color="blue white--text" @click="dialog = false">ОК</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-form>
   <Loader v-else/>
 </template>
@@ -27,7 +35,8 @@
       return {
         accountData: {},
         value: {},
-        getDataDone: false
+        getDataDone: false,
+        dialog: false
       }
     },
     methods: {
@@ -37,6 +46,7 @@
             if (answer) {
               store.dispatch('user/AC_UPDATE', this.value)
                 .then(res => {
+                  this.dialog = true;
                 });
             }
           });
